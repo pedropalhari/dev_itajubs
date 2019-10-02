c752e2dd8491680f1aab394b62bd25701a1de4a0
RobertoMenin <roberto_menin@hotmail.com>
2019-10-02 15:45:32 -0300

    inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-const App: React.FC = () => {
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#443737"
+      }}
+    >
+      <code>{"<dev/itajuba>"}</code>
     </div>
   );
 }
