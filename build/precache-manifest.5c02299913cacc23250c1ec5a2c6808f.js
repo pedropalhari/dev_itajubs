b4e695b042daa4f5eea66bbe4f498322b117d34f
Victor Brandão <victor.bradao37@gmail.com.br>
2019-10-03 22:18:05 -0300

    subindo build

---
+++ build/precache-manifest.5c02299913cacc23250c1ec5a2c6808f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c668215d66fa2cad5d8ff5103bf9374f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41143108d3e9187cd29d",
+    "url": "/static/css/main.518aec17.chunk.css"
+  },
+  {
+    "revision": "7b6e8ed9ada2eab0f9f5",
+    "url": "/static/js/2.a6d4a2d2.chunk.js"
+  },
+  {
+    "revision": "41143108d3e9187cd29d",
+    "url": "/static/js/main.4b1b719b.chunk.js"
+  },
+  {
+    "revision": "aec9d5fba33f27c0fde9",
+    "url": "/static/js/runtime-main.c18e45fe.js"
+  },
+  {
+    "revision": "6d54197aaaa13c2f6a66639cc17d888a",
+    "url": "/static/media/danilo.6d54197a.jpeg"
+  },
+  {
+    "revision": "49b86579a501b6c23a43513929817317",
+    "url": "/static/media/graphql.49b86579.jpg"
+  },
+  {
+    "revision": "7616c9059715d5ab694e62a4a59e56f5",
+    "url": "/static/media/gui_marques.7616c905.png"
+  },
+  {
+    "revision": "ebb668c30181eb612971aff16d87959e",
+    "url": "/static/media/nginx.ebb668c3.png"
+  },
+  {
+    "revision": "b015aabf829fb333f49b668352eccce6",
+    "url": "/static/media/pedro_palhari.b015aabf.jpg"
+  },
+  {
+    "revision": "cba0b89d2bf2d96a1ed26edb5849f804",
+    "url": "/static/media/react.cba0b89d.png"
+  },
+  {
+    "revision": "aae54b7438423cb275d5331b9a5966dc",
+    "url": "/static/media/react_shit.aae54b74.png"
+  },
+  {
+    "revision": "cf5f5198654f06343e2750b58a772b5f",
+    "url": "/static/media/roriz.cf5f5198.jpeg"
+  },
+  {
+    "revision": "ca95aaf4a7eec455499c94e760cf4675",
+    "url": "/static/media/the-day.ca95aaf4.jpeg"
+  }
+]);
